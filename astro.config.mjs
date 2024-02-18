5d02371ba908cfa13cff59f74e278f81e7e8f117
Agustin Acevedo <agus.acevedo2000@gmail.com>
2024-02-18 10:10:51 -0300

    cambio dominio

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://agusacevedo.github.io/'
+  site: 'https://agustinacevedo.me/'
 })
